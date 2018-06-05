1cc7eca595e433a39922425db1abfd0590eafc2d
varun <varunbalupuri@gmail.com>
2018-06-05 17:40:40 +0100

    setup, config and license

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'gtranslate',
+  packages = ['mypackage'],
+  version = '0.1',
+  description = 'pythonic interface to google translate services',
+  author = 'Varun Balupuri',
+  author_email = 'varunbalupuri@gmail.com',
+  url = 'https://github.com/varunbalupuri/gtranslate-python',
+  download_url = 'https://github.com/varunbalupuri/gtranslate-python/archive/0.1.tar.gz',
+  keywords = ['translation', 'google translate', 'translate'], # arbitrary keywords
+  classifiers = [],
+)
